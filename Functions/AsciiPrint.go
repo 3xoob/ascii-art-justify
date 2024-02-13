fe208962835ce72830ed614e115eed560dd6034f
aabdulhu <ali.almoumnin@gmail.com>
2024-02-13 19:42:23 +0300

    P5

--- Functions/AsciiPrint.go
+++ Functions/AsciiPrint.go
@@ -20,7 +20,7 @@ func AsciiPrint(s string, font string, align string) []string {
 	// The function initializes an array called charArray using the
 	// initializeLines function, which determines the number of lines needed
 	// based on the validity of the characters in the input string s.
-	
+
 	charArray := initializeLines(s)
 	// Loop through each character in the string
 	for i := 0; i < len(s); i++ {
@@ -56,7 +56,6 @@ func AsciiPrint(s string, font string, align string) []string {
 			fmt.Println(result)
 		} else {
 			fmt.Println(line)
-
 		}
 	}
 
