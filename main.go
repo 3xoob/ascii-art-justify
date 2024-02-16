ccad4b1fbd27a68da7609a38c6a5be0a43346599
aabdulhu <ali.almoumnin@gmail.com>
2024-02-16 22:11:51 +0300

    P5

--- main.go
+++ main.go
@@ -29,9 +29,9 @@ func main() {
 			os.Exit(0)
 		}
 		if align == "justify" {
-			Justify.JustifyPrinter(strings.ReplaceAll(os.Args[2], `\n`, "\n"), os.Args[3], align)
+			Justify.JustifyPrinter(strings.ReplaceAll(strings.ReplaceAll(os.Args[2], "\\t", "    "), `\n`, "\n"), os.Args[3], align)
 		} else {
-			Justify.AsciiPrint(strings.ReplaceAll(os.Args[2], `\n`, "\n"), os.Args[3], align)
+			Justify.AsciiPrint(strings.ReplaceAll(strings.ReplaceAll(os.Args[2], "\\t", "    "), `\n`, "\n"), os.Args[3], align)
 		}
 	} else if len(os.Args[1:]) == 2 {
 		if strings.HasPrefix(os.Args[1], "--align=") {
@@ -42,12 +42,12 @@ func main() {
 				os.Exit(0)
 			}
 			if align == "justify" {
-				Justify.JustifyPrinter(strings.ReplaceAll(os.Args[2], `\n`, "\n"), "standard", align)
+				Justify.JustifyPrinter(strings.ReplaceAll(strings.ReplaceAll(os.Args[2], "\\t", "    "), `\n`, "\n"), "standard", align)
 			} else {
-				Justify.AsciiPrint(strings.ReplaceAll(os.Args[2], `\n`, "\n"), "standard", align)
+				Justify.AsciiPrint(strings.ReplaceAll(strings.ReplaceAll(os.Args[2], "\\t", "    "), `\n`, "\n"), "standard", align)
 			}
 		} else if os.Args[2] == "standard" || os.Args[2] == "shadow" || os.Args[2] == "thinkertoy" {
-			Justify.AsciiPrint(strings.ReplaceAll(os.Args[1], `\n`, "\n"), os.Args[2], "")
+			Justify.AsciiPrint(strings.ReplaceAll(strings.ReplaceAll(os.Args[1], "\\t", "    "), `\n`, "\n"), os.Args[2], "")
 		} else {
 			fmt.Println("ERROR:")
 			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\nEX: go run . --align=right \"something\" standard")
@@ -56,7 +56,7 @@ func main() {
 			os.Exit(0)
 		}
 	} else if len(os.Args[1:]) == 1 {
-		Justify.AsciiPrint(strings.ReplaceAll(os.Args[1], `\n`, "\n"), "standard", "")
+		Justify.AsciiPrint(strings.ReplaceAll(strings.ReplaceAll(os.Args[1], "\\t", "    "), `\n`, "\n"), "standard", "")
 	} else {
 		fmt.Println("ERROR:")
 		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\nEX: go run . --align=right \"something\" standard")
